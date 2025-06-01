c2cadd3662c47c3235a7392c263ebd344d47aa10
nabilaadou <nabilaadou@gmail.com>
2025-06-01 16:42:15 +0100

    trying to setup docker containers for each service

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from cppTreeGenerator import cppGenerateTree
+import sys
+sys.path.append('./app')
 from pyTreeGenerator import pyGenerateTree
 
 app = FastAPI()
@@ -12,12 +13,6 @@ app.add_middleware(
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"]   # Allow all headers like Content-Type
 )
-
-@app.post("/cppSourceFiles")
-async def root(request : Request):
-	projectFiles = await request.json()
-	response = cppGenerateTree(projectFiles)
-	return response
 
 @app.post("/pySourceFiles")
 async def root(request : Request):
