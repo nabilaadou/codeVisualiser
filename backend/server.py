7d21f105bd42002cfaff9aa9e444f44fb2473984
nabilaadou <nabilaadou@gmail.com>
2025-06-02 21:04:57 +0100

    settin up the docker env

--- backend/server.py
+++ backend/server.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],  # Frontend origin
+    allow_origins=["*"],  # Frontend origin
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"]   # Allow all headers like Content-Type
@@ -19,3 +19,5 @@ async def root(request : Request):
 	projectFiles = await request.json()
 	response = pyGenerateTree(projectFiles)
 	return response
+
+
